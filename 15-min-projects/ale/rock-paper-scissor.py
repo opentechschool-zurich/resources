20b3ecc5683bec054534bf3acec46a3e9837bb0f
ale rimoldi <ale@graphicslab.org>
2023-05-11 11:58:48 +0200

    add solutions with a list and a dict

--- 15-min-projects/ale/rock-paper-scissor.py
+++ 15-min-projects/ale/rock-paper-scissor.py
@@ -1,4 +1,4 @@
-i# scissor - rock -paper
+# scissor - rock -paper
 
 import random
 
@@ -22,3 +22,26 @@ elif (human_move == 'rock' and computer_move == 'scissor' or
 else:
     print('computer wins')
 
+# Instead of comparing each move by text, we can build the list of winning moves for the human
+
+human_wins = [2, 0, 1] # for 0..2 values of human, computer values that make the human win
+# computer_i = random.randint(0, 2) # if we go for the index, no need to set the computer move as a string
+computer_i = moves.index(computer_move)
+human_i = moves.index(human_move)
+print(computer_i, human_i)
+if human_i == computer_i:
+    print('draw')
+elif human_wins[human_i] == computer_i:
+    print('human wins')
+else:
+    print('computer wins')
+
+# Or, for readability, use a dict...
+
+human_move_wins = {'scissor': 'paper', 'rock': 'scissor', 'paper': 'rock'}
+if human_move == computer_move:
+    print('draw')
+elif human_move_wins[human_move] == computer_move:
+    print('human wins')
+else:
+    print('computer wins')
